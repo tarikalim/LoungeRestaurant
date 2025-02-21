1ec874bd17d92132f2b145e62c998554dfeae749
tarik <tarikalim1899@gmail.com>
2025-02-21 18:50:38 +0300

    missing files added.

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CommentPage from './page/CommentPage';
+
+function App() {
+    return (
+        <div>
+            <CommentPage />
+        </div>
+    );
+}
+
+export default App;
